89ebb7c24f40e8ab07b5f195a8d0936ecf995d7d
Shimpi <shimpi.shimpi@ibm.com>
2024-10-28 01:32:59 +0530

    t

--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "ecr-repo" {
-  name                 = var.app_name
+  name                 = terraform-project
   image_tag_mutability = "MUTABLE"
  
   image_scanning_configuration {
