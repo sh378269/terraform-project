d9a33f9a31b38a58a22a5f475722c478e79f1d3d
Shimpi <shimpi.shimpi@ibm.com>
2024-10-28 00:05:31 +0530

    tg

--- .github/workflows/S3/s3.tf
+++ .github/workflows/S3/s3.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "tf-file" {
-  bucket = "shimpi-dev"
-  tags = {
-    description = "tf state file adding"
+#resource "aws_s3_bucket" "tf-file" {
+#  bucket = "shimpi-dev"
+#  tags = {
+#    description = "tf state file adding"
 
-  }
-  }
+#  }
+#  }
