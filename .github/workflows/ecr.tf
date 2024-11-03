411e837e16b84f440762879c761ac0c9deecd0bb
Shimpi <shimpi.shimpi@ibm.com>
2024-11-04 01:27:54 +0530

    msg

--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -61,7 +61,7 @@ resource "aws_s3_bucket" "tf-file" {
 }
 resource "aws_s3_bucket_object" "shared"{
     bucket  = "shimpi-dev"
+    acl = "private"
     key     = "shared/"
-    content_type = "shared/terraform.statetf"
     source  = var.tfpath
 }
