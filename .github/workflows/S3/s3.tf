693cb0b3875b1e28987f8e8d3aa91bced05027bb
Shimpi <shimpi.shimpi@ibm.com>
2024-11-03 22:45:32 +0530

    msg

--- .github/workflows/S3/s3.tf
+++ .github/workflows/S3/s3.tf
@@ -7,8 +7,8 @@ resource "aws_s3_bucket" "tf-file" {
   }
   }
 resource "aws_s3_bucket_object" "shared"{
-    bucket = aws_s3_bucket.tf-file.id
-    key = "terraform.statetf"
-    source = "/home/runner/work/terraform-project/terraform-project/.github/workflows/terraform.tfstate"
+    bucket  = aws_s3_bucket.tf-file.id
+    key     = "shared/terraform.statetf"
+    source  = var.tfpath
 }
 
