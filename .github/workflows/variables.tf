e3612325e4254af1f5881c288eda161962808b01
Shimpi <shimpi.shimpi@ibm.com>
2024-11-14 19:07:19 +0530

    msg

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -1,5 +1,5 @@
 variable "tfpath" {
   type        = string
-  default     = "/home/runner/work/terraform-project/terraform-project/.github/workflows/"
+  default     = "/home/runner/work/terraform-project/terraform-project/.github/workflows"
   description = "The path of our statefile"
 }
