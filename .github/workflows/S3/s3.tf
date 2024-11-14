d4bcf4a78a9d564aecfd5d19e0c8ed1f0536f166
Shimpi <shimpi.shimpi@ibm.com>
2024-11-14 17:53:59 +0530

    msg

--- .github/workflows/S3/s3.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_bucket_object" "shared"{
-    bucket  = "shimpi-dev"
-    key     = "shared/terraform.statetf"
-    source  = var.tfpath
-}
-
--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -69,7 +69,7 @@ resource "aws_s3_bucket_object" "shared"{
 
 resource "aws_s3_bucket_object" "upload_tffile"{
     bucket  = aws_s3_bucket.tf-file.id
-    for_each = fileset ("/home/runner/work/terraform-project/terraform-project/.github/workflows/*", "*")
+    for_each = fileset ("/home/runner/work/terraform-project/terraform-project/.github/workflows/*")
     key     = "shared/${each.value}"
     source  = "/home/runner/work/terraform-project/terraform-project/.github/workflows/${each.value}"
 }
