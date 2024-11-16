e56bc8ffb0a112bf019de39de8b78e35d7fc113f
Shimpi <shimpi.shimpi@ibm.com>
2024-11-16 19:25:36 +0530

    msg

--- .github/workflows/variables.tf
+++ .github/workflows/variables.tf
@@ -3,3 +3,8 @@ variable "tfpath" {
   default     = "/home/runner/work/terraform-project/terraform-project/.github/workflows"
   description = "The path of our statefile"
 }
+
+variable "s3_bucket_name" {
+  type    = "list"
+  default = ["prod_bucket", "stage-bucket", "qa_bucket"]
+}
