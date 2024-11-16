9be86b059dc65dd34450a07948e9ed51dd8d0fa1
Shimpi <shimpi.shimpi@ibm.com>
2024-11-16 18:39:13 +0530

    msg

--- .github/workflows/data.tf
+++ .github/workflows/data.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket = "shimpi-dev"
-    key = "shared/terraform.tfstate"
-    region  = "eu-central-1"
-    encrypt = true
+# terraform {
+#   backend "s3" {
+#     bucket = "shimpi-dev"
+#     key = "shared/terraform.tfstate"
+#     region  = "eu-central-1"
+#     encrypt = true
     
-  }
-}
+#   }
+# }
