e6daadd3e46143e70e60fd6ec03b076be8af21b9
Shimpi <shimpi.shimpi@ibm.com>
2024-11-16 19:04:42 +0530

    msg

--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -1,9 +1,9 @@
 resource "aws_ecr_repository" "ecr-repo" {
   name                 = "terraform-project2"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
  
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = false
   }
   encryption_configuration {
     encryption_type = "KMS"
