eac9aebadbe68bbb8b552617c55398f754388e0a
Shimpi <shimpi.shimpi@ibm.com>
2024-11-16 20:15:53 +0530

    msg

--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -52,7 +52,7 @@ EOF
 }
 
 
-resource "aws_s3_bucket" "tf-file" {
+resource "aws_s3_bucket" "files" {
    count         = "${length(var.s3_bucket_name)}"
    bucket        = "${var.s3_bucket_name[count.index]}"
    acl           = "private"
