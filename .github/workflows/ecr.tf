fe004ccdf63788812e64a6dde8ed5f026688891a
Shimpi <shimpi.shimpi@ibm.com>
2024-11-17 11:16:09 +0530

    msg

--- .github/workflows/ecr.tf
+++ .github/workflows/ecr.tf
@@ -1,80 +1,8 @@
-resource "aws_ecr_repository" "ecr-repo" {
-  name                 = "terraform-project2"
-  image_tag_mutability = "MUTABLE"
- 
-  image_scanning_configuration {
-    scan_on_push = false
-  }
-  encryption_configuration {
-    encryption_type = "KMS"
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 17.0.0"
+    }
   }
 }
- 
-#resource "aws_ecr_repository_policy" "ecr-repo-permission" {
-#  repository = aws_ecr_repository.ecr-repo.name
-#  policy     = data.terraform_remote_state.shared.outputs.ecrpermission-json
-#}
- 
-resource "aws_ecr_lifecycle_policy" "ecr-repo-lifeCyclepolicy" {
-  repository = aws_ecr_repository.ecr-repo.name
- 
-  policy = <<EOF
-{
-  "rules": [
-    {
-      "rulePriority": 1,
-      "description": "Expire untagged images in 1 day.",
-      "selection": {
-        "tagStatus": "untagged",
-        "countType": "sinceImagePushed",
-        "countUnit": "days",
-        "countNumber": 1
-      },
-      "action": {
-        "type": "expire"
-      }
-    },
-    {
-      "rulePriority": 2,
-      "description": "Keep latest tagged images.",
-      "selection": {
-        "tagStatus": "any",
-        "countType": "imageCountMoreThan",
-        "countNumber": 5
-      },
-      "action": {
-        "type": "expire"
-      }
-    }
-  ]
-}
-EOF
-}
-
-
-resource "aws_s3_bucket" "files" {
-   count         = "${length(var.s3_bucket_name)}"
-   bucket        = "${var.s3_bucket_name[count.index]}"
-   acl           = "private"
-   tags = {
-     description = "used for Testing changes env ${var.s3_bucket_name[count.index]}"
-
-    }
-}
-resource "aws_s3_bucket_object" "shared"{
-    count   = "${length(var.s3_bucket_name)}"
-    bucket  = aws_s3_bucket.files[count.index].id
-    acl     = "private"
-    key     = "shared/"
-   #content type is kind of type of data you are uploading like image, file etc 
-    content_type  = "application/x-directory"  
-}
-
-# uploading multiple files from some path
-# resource "aws_s3_bucket_object" "upload_tffile"{
-#     bucket  = aws_s3_bucket.tf-file.id
-#     for_each =  fileset("${var.tfpath}/","*")
-#     key     = "shared/${each.value}"
-#     source  = each.value
-# }
-
